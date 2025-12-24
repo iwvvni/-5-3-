9309e15697c4b3ad59cf77228bf81a97930c991a
iwvvni <vikasemenshenko@mail.ru>
2025-12-24 03:47:12 +0300

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,7 +1,9 @@
 #ifndef UTILS_H
 #define UTILS_H
+
 #include "data.h"
 
+// Вспомогательные функции
 void clear_input_buffer(void);
 int get_int_input(const char* prompt, int min, int max);
 void get_string_input(const char* prompt, char* buffer, int max_len);
@@ -9,6 +11,6 @@ void print_separator(void);
 void print_video_detail(const Video* video);
 void press_enter_to_continue(void);
 void convert_duration(int seconds, int* minutes, int* secs);
-bool validate_date_format(const char* date); // НОВАЯ ФУНКЦИЯ
+bool validate_date_format(const char* date);
 
 #endif
